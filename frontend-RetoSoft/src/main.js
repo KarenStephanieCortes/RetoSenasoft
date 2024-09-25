c67954fece11cc8b8fbad324c668ff5581b47d81
KarenStephanieCortes <diazricostephanie7@gmail.com>
2024-09-25 14:00:20 -0500

    pagina de inicio

--- frontend-RetoSoft/src/main.js
+++ frontend-RetoSoft/src/main.js
@@ -7,9 +7,11 @@ import App from './App.vue'
 import { router } from './routes'
 import { createPinia } from 'pinia'
 import piniaPersist from 'pinia-plugin-persistedstate';
+import { VueReCaptcha} from 'vue-recaptcha-v3';
 const pinia = createPinia();
 pinia.use(piniaPersist);
 createApp(App)
     .use(router)
     .use(pinia)
+    .use(VueReCaptcha,{siteKey:"6LemM04qAAAAANyCVgoXAoQL2GQL876W0_O_BBjj"})
     .mount('#app')
