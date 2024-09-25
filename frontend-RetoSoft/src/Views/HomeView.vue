c040f66d97d0597e20d29d95e94ed731e0dc293c
KarenStephanieCortes <diazricostephanie7@gmail.com>
2024-09-25 13:02:52 -0500

    homeView

--- frontend-RetoSoft/src/Views/HomeView.vue
+++ frontend-RetoSoft/src/Views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row mt-2 h-100 bg-secondary rounded justify-content-center mx-0">
-    <div class="row bg-primary m-4 w-100">
+    <div class="row bg-primary m-4">
       
     </div>
     <div class="row"></div>
